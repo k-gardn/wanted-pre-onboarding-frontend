dfd0a9659e6d6025b7f0305502a80ba5d33dc788
jeongwon <rladnjs15@naver.com>
2022-12-11 14:48:57 +0900

    Feat: 로그인 회원가입 기능 구현

---
+++ src/pages/TodoList.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { TodoItem } from "../components/todo/TodoItem";
+
+export const TodoList = () => {
+  // 리스트 페이지에는 입력창과 추가 버튼이 있고, 추가 버튼을 누르면 입력창의 내용이 새로운 투두 리스트로 추가되도록 해주세요
+  const [todo]
+  
+  return (
+    <>
+      <div>todo List</div>
+      <input
+          placeholder="오늘의 할일은 무엇이 있을까?"
+          title="password"
+          type="password"
+          value={password}
+          onChange={pwHandler}
+        ></input>
+      <TodoItem />
+      <TodoItem />
+      <TodoItem />
+    </>
+  );
+};
